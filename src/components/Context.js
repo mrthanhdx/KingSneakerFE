40530f01979d8a49ff03dcf53ab5e29a3bd6c453
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-09-22 19:44:16 +0700

    update 22/9

--- src/components/Context.js
+++ src/components/Context.js
@@ -3,7 +3,7 @@ import {createContext, useContext,useState} from 'react';
 export const GContext = createContext();
 
 export function ContextProvider({children}){
-    const [currentUI,setCurrentUI] = useState("ProductDetails");
+    const [currentUI,setCurrentUI] = useState("SellAtCounter");
     const [sellCounterScreen,setSellCounterScreen] = useState("listInvoice");
     const [invoiceDetail,setInvoiceDetail] = useState(null);
     
--- src/components/counterSell/DetailInvoice.js
+++ src/components/counterSell/DetailInvoice.js
@@ -1,18 +1,35 @@
 import { useContext } from "react";
 import { GContext } from "../Context";
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-function DetailInvoice({idInvoice}){
 
-    const {sellCounterScreen, setSellCounterScreen} = useContext(GContext);
+
+function DetailInvoice({ idInvoice }) {
+    library.add(fas);
+    const { sellCounterScreen, setSellCounterScreen } = useContext(GContext);
     return (
-
         <>
             <h1>
                 Invoice : {idInvoice}
             </h1>
-            <button onClick={()=>{
-                setSellCounterScreen("listInvoice")
-            }}>Back to List Invoice</button>
+            <button
+                style={{ width: "120px", height: "40px", border: "none", borderRadius: "10px", position: "absolute", left: "30px", top: "auto" }}
+                onClick={() => {
+                    setSellCounterScreen("listInvoice")
+                }}
+            > <FontAwesomeIcon icon="fa-solid fa-arrow-left" /> Back</button>
+
+            <div>
+                <FontAwesomeIcon icon="check-square" />
+                Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
+                
+            </div>
+
+            
         </>
     )
 }
